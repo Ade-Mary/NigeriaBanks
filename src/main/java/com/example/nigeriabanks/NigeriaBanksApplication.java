542a56f3bee7ecdb3de99cd62c54e8091acf55c6
Mary Adewunmi <physayoemma@gmail.com>
2024-08-19 18:00:26 +0100

    External api

---
+++ src/main/java/com/example/nigeriabanks/NigeriaBanksApplication.java
@@ -0,0 +1,13 @@
+package com.example.nigeriabanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NigeriaBanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NigeriaBanksApplication.class, args);
+	}
+
+}
